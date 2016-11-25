19cb50646d0f12b86b61503092e8a0a4848e610d
floraCat <1781721016@qq.com>
2016-11-25 18:29:23 +0800

    11

--- _res1.php
+++ _res1.php
@@ -6,6 +6,13 @@ $rs="第一页rs";
 
 $smarty->assign("rs",$rs);
 
+/*-------------------------头部信息----------*/
+echo("<pre>");
+print_r($_SERVER['HTTP_X_PJAX']);
+//print_r(getallheaders());
+echo("</pre>");
+/*-----------------------------------*/
+
 $smarty->display("_res1.html");
 
 ?>
