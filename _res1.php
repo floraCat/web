efcef9e173c65e729bf8000bdb6d5ed8b900e512
floraCat <1781721016@qq.com>
2016-11-28 18:33:59 +0800

    11-28

--- _res1.php
+++ _res1.php
@@ -8,9 +8,17 @@ $smarty->assign("rs",$rs);
 
 /*-------------------------头部信息----------*/
 echo("<pre>");
-print_r($_SERVER['HTTP_X_PJAX']);
+print_r($_SERVER);
+//print_r($_SERVER['HTTP_X_PJAX']);
 //print_r(getallheaders());
 echo("</pre>");
+
+
+// function is_pjax(){
+//     return array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX'];
+// }
+
+
 /*-----------------------------------*/
 
 $smarty->display("_res1.html");
